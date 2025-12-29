9c64f4edf45a77dabeabada855a257a9a719e8f5
Pratik <devlopwithpratik@gmail.com>
2025-12-29 14:04:42 +0530

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -3,12 +3,12 @@ import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut, sendP
 import { initializeFirestore, collection, addDoc, getDocs, getDoc, setDoc, doc, updateDoc, deleteDoc, query, orderBy, where } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBMHQiHK_6Vyn1SFGWF41llNbwbwaCsj18",
-  authDomain: "nssgecb-ef451.firebaseapp.com",
-  projectId: "nssgecb-ef451",
-  storageBucket: "nssgecb-ef451.firebasestorage.app",
+  apiKey: "Enter Your Api Key",
+  authDomain: "yourdomain.firebaseapp.com",
+  projectId: "yourproject",
+  storageBucket: "your/storage.firebasestorage.app",
   messagingSenderId: "510721207564",
-  appId: "1:510721207564:web:fff3f6d2cc2bdfcaf3ed29"
+  appId: "1:5107212iiyhdnd64:web:fff3f6d2cc2bdfcaf3ed29"
 };
 
 const app = initializeApp(firebaseConfig);
